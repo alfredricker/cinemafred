df7c9a15bd1fe93970f108e8353bb03daeaf9597
alfred ricker <alfred@DESKTOP-0HFTRBR.myfiosgateway.com>
2025-01-09 18:50:50 -0500

    config.mjs update

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const config = {    
+    images: {
+      domains: [],
+    },
+  
+    typescript: {
+      ignoreBuildErrors: false,
+    },
+  
+    reactStrictMode: true,
+  };
+  
+  export default config;
