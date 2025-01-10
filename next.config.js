db704304178ce2bae4bef3add5f3a1b8442ed12e
alfred ricker <alfred@DESKTOP-0HFTRBR.myfiosgateway.com>
2025-01-10 14:50:17 -0500

    vercel update

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: []
+  },
+
+  typescript: {
+    ignoreBuildErrors: false
+  },
+
+  reactStrictMode: true
+}
+
+module.exports = nextConfig
